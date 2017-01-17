76420ab6271fc10ead9482b1eaf052977927e7cb
TomCreeper <tompaszylk@gmail.com>
2017-01-17 13:30:23 +0000

    Done
    Done!

--- commands/default/pong.js
+++ commands/default/pong.js
@@ -2,11 +2,11 @@ const Oxyl = require("../../oxyl.js"),
 	Command = require("../../modules/commandCreator.js"),
 	framework = require("../../framework.js");
 
-var command = new Command("ping", async (message, bot) => {
+var command = new Command("pong", async (message, bot) => {
 	let time = Date.now();
-	let msg = await message.channel.createMessage("Pong!");
-	msg.edit(`Pong! \`${Date.now() - time}ms\``);
+	let msg = await message.channel.createMessage("Ping!");
+	msg.edit(`Ping! \`${Date.now() - time}ms\``);
 }, {
 	type: "default",
-	description: "Test the bot's responsiveness"
+	description: "Addon to ping command!"
 });
